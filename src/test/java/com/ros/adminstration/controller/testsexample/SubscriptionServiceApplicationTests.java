dd1a4be67cadfe50eb7159f7e619b43745888218
subhash.mb <subhash.mb@skillassure.com>
2022-10-03 14:22:37 +0000

    Update SubscriptionServiceApplicationTests.java

--- src/test/java/com/ros/adminstration/controller/testsexample/SubscriptionServiceApplicationTests.java
+++ src/test/java/com/ros/adminstration/controller/testsexample/SubscriptionServiceApplicationTests.java
@@ -1,26 +1,26 @@
 package com.ros.adminstration.controller.testsexample;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.test.web.servlet.MockMvc;
 
-import com.ros.administration.controller.SubscriptionController;
-import com.ros.administration.repository.SubscriptionRepository;
-import com.ros.administration.service.impl.SubscriptionServiceImpl;
+// import com.ros.administration.controller.SubscriptionController;
+// import com.ros.administration.repository.SubscriptionRepository;
+// import com.ros.administration.service.impl.SubscriptionServiceImpl;
 
-public class SubscriptionServiceApplicationTests {
+// public class SubscriptionServiceApplicationTests {
 
-    @Autowired
-    MockMvc mvc;
+//     @Autowired
+//     MockMvc mvc;
     
-    @MockBean
-    SubscriptionServiceImpl serviceimpl;
+//     @MockBean
+//     SubscriptionServiceImpl serviceimpl;
     
-    @MockBean
-    SubscriptionRepository repo;
+//     @MockBean
+//     SubscriptionRepository repo;
     
-    @MockBean
-    SubscriptionController controller;
+//     @MockBean
+//     SubscriptionController controller;
     
     
-}
+// }
